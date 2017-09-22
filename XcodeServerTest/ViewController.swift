0e9cff94ecadc51dccb8962c83604f7017584500
kazaura <kazaura@infocity.co.jp>
2017-09-22 22:30:52 +0900

    Initial Commit

---
+++ XcodeServerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeServerTest
+//
+//  Created by kazaura on 2017/09/22.
+//  Copyright © 2017年 kazaura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
